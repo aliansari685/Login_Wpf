cc19211322c0a062338e2ee705d5c78df96679e3
Ali Ansari <aliansari685@gmail.com>
2023-10-09 00:04:50 +0330

    Add Files

---
+++ WpfApp/Classes/Checkup.cs
@@ -0,0 +1,119 @@
+﻿using HandyControl.Tools.Extension;
+using ShowMsg;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+using static ShowMsg.ShowMessage;
+
+namespace WpfApp
+{
+    public static class Checkup
+    {
+        private static ShowMessage MessageBox = new ShowMsg.ShowMessage
+        {
+            FontFamily = new System.Windows.Media.FontFamily("B Badr"),
+            MessageFontSize = 21,
+            IconColor = "#202A44",
+        };
+
+        //تعریف نوع داده شمارشی جهت نمایش پیام های دلخواه و مقدار دهی آن در خط بعدی
+        public enum Error { Null, Tekrari, Ok, Cancel, DataBase, Pass, Format, Login, Other }
+
+        /// <summary>
+        /// This is a Cool DialogBox Create With Material Design
+        /// </summary>
+        /// <param name="Foul"></param>
+        /// <param name="Message"></param>
+        public static bool Print(string Message) => MessageBox.Show(Message, "Result", ShowMsgButtons.YesNo, ShowMsgIcon.Warning) == ShowMsgResult.Yes;
+        /// <summary>
+        /// This is a Cool DialogBox Create With Material Design
+        /// </summary>
+        /// <param name="Foul"></param>
+        /// <param name="Message"></param>
+        public static void Print(Error Foul, string Message)
+        {
+            ShowMessage MessageBox = new ShowMsg.ShowMessage
+            {
+                FontFamily = new System.Windows.Media.FontFamily("B Badr"),
+                MessageFontSize = 21,
+                IconColor = "#202A44",
+            };
+            switch (Foul)
+            {
+                case Error.Null:
+                    MessageBox.Show("مقادیر ورودی خالی است" + Message, "Message", ShowMsgButtons.Ok, ShowMsgIcon.Error);
+                    break;
+
+                case Error.Tekrari:
+                    MessageBox.Show("مقدار نام کاربری تکرای است" + Message, "Message", ShowMsgButtons.Ok, ShowMsgIcon.Error);
+                    break;
+
+                case Error.Ok:
+                    MessageBox.Show("عملیات با موفقیت انجام شد" + Message, "Message", ShowMsgButtons.Ok, ShowMsgIcon.Success);
+                    break;
+
+                case Error.Cancel:
+                    MessageBox.Show("عملیات متوقف و انجام نشد" + Message, "Message", ShowMsgButtons.Ok, ShowMsgIcon.Warning);
+                    break;
+
+                case Error.DataBase:
+                    MessageBox.Show("مشکلی در پایگاه داده بوجود آمد" + Message, "Message", ShowMsgButtons.Ok, ShowMsgIcon.Warning);
+                    break;
+
+                case Error.Pass:
+                    MessageBox.Show("رمز عبور از پنج حرف کمتر یا یکسان نیست" + Message, "Message", ShowMsgButtons.Ok, ShowMsgIcon.Error);
+                    break;
+
+                case Error.Format:
+                    MessageBox.Show("فرمت مقادیر ورودی معتبر نیست" + Message, "Message", ShowMsgButtons.Ok, ShowMsgIcon.Error);
+                    break;
+
+                case Error.Login:
+                    MessageBox.Show("نام کاربری  یا رمز عبور اشتباه است" + Message, "Message", ShowMsgButtons.Ok, ShowMsgIcon.Error);
+                    break;
+
+                case Error.Other:
+                    MessageBox.Show(Message, "Other", ShowMsgButtons.Ok, ShowMsgIcon.Warning);
+                    break;
+            }
+        }
+        public static async Task<string> Hash(string data, string key)
+        {
+            await Task.Delay(1000);
+            using (HMACSHA256 hmac = new HMACSHA256(Encoding.UTF8.GetBytes(key)))
+            {
+                byte[] hashBytes = hmac.ComputeHash(Encoding.UTF8.GetBytes(data));
+                return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+            }
+        }
+        public static string Shamsi_Date()
+        {
+            PersianCalendar pr = new PersianCalendar();
+            string datenow = string.Format("{0}/{1}/{2}-{3}",
+                pr.GetYear(DateTime.Now),
+                pr.GetMonth(DateTime.Now),
+                pr.GetDayOfMonth(DateTime.Now),
+                DateTime.Now.ToLongTimeString());
+            return datenow;
+        }
+        #region Checking Functions
+        //در این توابع چک کردن فرمت ورودی و خالی بودن آن ها بررسی میشود
+        public static bool Check_Input(params string[] str) => str.Any(x => string.IsNullOrWhiteSpace(x) || Regex.IsMatch(x, @"\s"));
+        public static bool Check_Password(string str, string str1) => (str == str1) && (str.Length >= 5) && !(string.IsNullOrWhiteSpace(str));
+        public static bool Check_Mail(string str)
+        {
+            string pattern = @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,4})+)";
+            return Regex.IsMatch(str, pattern);
+        }
+        public static bool IsPersianText(string text) => text.Any(c => c >= 0x0600 && c <= 0x06FF);
+        public static bool IsEnglishText(string text) => text.All(x => char.IsLetter(x));
+        //   public static bool IsText(string text) => IsPersianText(text) || IsEnglishText(text);
+        #endregion
+    }
+}
---
+++ WpfApp/Forms/frm3.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using WpfApp.Model;
+using static WpfApp.Checkup;
+
+namespace WpfApp.Forms
+{
+    /// <summary>
+    /// Interaction logic for frm3.xaml
+    /// </summary>
+    /// 
+    public partial class frm3 : Window, IDisposable
+    {
+        private readonly Database db;
+        private List<string> Items;
+        private User U;
+        private int? IU;
+        public int? Insert_update
+        {
+            get => IU;
+            set
+            {
+                using (var context = new LoginDBEntities())
+                    if (context.Users.Any(x => x.ID == value))
+                    {
+                        IU = value;
+                        U = context.Users.Find(value);
+                        Replace();
+                    }
+            }
+        }
+        public frm3()
+        {
+            InitializeComponent();
+            db = new Database();
+            datepicker1.Text = Shamsi_Date().Remove(Shamsi_Date().IndexOf('-'));
+            Firstname_Box.Focus();
+        }
+        private async void RegisterBTN_Click(object sender, RoutedEventArgs e)
+        {
+            string mdfy = (bool)(write_radio.IsChecked) ? "write" : "read";
+            if (Check_Input(Firstname_Box.Text, Lastname_Box.Text, Username_Box.Text, Mail_Box.Text))
+            {
+                Print(Error.Null, null);
+                return;
+            }
+            if (!Check_Mail(Mail_Box.Text) || !(IsEnglishText(Firstname_Box.Text)) || !(IsEnglishText(Lastname_Box.Text)))
+            {
+                Print(Error.Format, null);
+                return;
+            }
+            if (IU != null) //Update:
+            {
+                int id = (int)IU;
+                Items = new List<string> { Firstname_Box.Text, Lastname_Box.Text, Username_Box.Text, Mail_Box.Text, mdfy, datepicker1.Text };
+                if (db.UpdateTBL(id, Items))
+                {
+                    Print(Error.Ok, null);
+                    this.Close();
+                }
+            }
+            else   //Insert:
+            {
+                if (!Check_Password(Password_Box.Password, Password_Box2.Password))
+                {
+                    Print(Error.Pass, null);
+                    return;
+                }
+                Items = new List<string> { Firstname_Box.Text, Lastname_Box.Text, Username_Box.Text, Mail_Box.Text, Password_Box.Password, "read", datepicker1.Text };
+                if (await db.InsertToDB(Items))
+                {
+                    Print(Error.Ok, "");
+                    this.Close();
+                }
+            }
+        }
+        private void datepicker1_CalendarClosed(object sender, RoutedEventArgs e) => Print(Error.Other, "توجه : تاریخ تولد انتخاب شده باید افراد متولدین بیشتر از 15 سال باشند\r\nدر غیر اینصورت در پایگاه داه خالی ثبت خواهد شد ");
+        public void Dispose() => GC.SuppressFinalize(this);
+        private void Closing_Click(object sender, RoutedEventArgs e) => this.Close();
+        private void Replace()
+        {
+            Firstname_Box.Text = U.Firstname;
+            Lastname_Box.Text = U.Lastname;
+            Username_Box.Text = U.Username;
+            Mail_Box.Text = U.Mail;
+            datepicker1.Text = U.Birth;
+            write_radio.IsChecked = U.Modify == "write";
+            Password_Box.IsEnabled = Password_Box2.IsEnabled = false;
+        }
+    }
+}
